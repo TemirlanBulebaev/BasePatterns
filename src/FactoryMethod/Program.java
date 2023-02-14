9c62e9fd487c6295cb84ce3c3d72582f2725fcc0
T <tbulebaev@bk.ru>
2023-02-14 21:44:46 +0500

    FactoryMethod

--- src/FactoryMethod/Program.java
+++ src/FactoryMethod/Program.java
@@ -2,6 +2,22 @@ package FactoryMethod;
 
 public class Program {
     public static void main(String[] args) {
+        DeveloperFactory developerFactory = createDeveloperBySpeciality("java");
+        Developer developer = developerFactory.createDeveloper();
+        developer.writeCode();
 
+    }
+
+    static DeveloperFactory createDeveloperBySpeciality(String specialty) {
+        if (specialty.equalsIgnoreCase("java")){
+            return new JavaDeveloperFactory();
+        } else if (specialty.equalsIgnoreCase("c++")){
+            return new CppDeveloperFactory();
+        } else if(specialty.equalsIgnoreCase("php")){
+            return new PhpDeveloperFactory();
+        }
+        else {
+            throw new RuntimeException(specialty+ " is unknown speciality!");
+        }
     }
 }
