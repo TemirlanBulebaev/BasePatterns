982e43094aae4c9aa45d0f8d2ee870685fac57be
T <tbulebaev@bk.ru>
2023-02-15 17:13:07 +0500

    Adapter

---
+++ src/Adapter/JavaApplication.java
@@ -0,0 +1,19 @@
+package Adapter;
+
+public class JavaApplication {
+    public void saveObject(){
+        System.out.println("Saving Java Object...");
+    }
+
+    public void updateObject(){
+        System.out.println("Update Java Object...");
+    }
+
+    public void loadObject(){
+        System.out.println("Loading Java Object...");
+    }
+
+    public void deleteObject(){
+        System.out.println("Delete Java Object...");
+    }
+}
